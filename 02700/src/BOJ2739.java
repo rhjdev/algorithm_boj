c9f3ea85fd490902b4ad4135d627a27f4d8f6fdf
rhjdev <rhjdev@gmail.com>
2022-02-13 22:35:28 +0900

    Mathematics, 구구단

---
+++ 02700/src/BOJ2739.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BOJ2739 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=n; i == n; i++) {
+			for(int j=1; j < 10; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j));
+			}
+		}
+	}
+}
