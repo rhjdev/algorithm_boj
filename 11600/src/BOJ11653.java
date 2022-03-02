47c31298271e22c8043ca32b5d734ca000d327b3
rhjdev <rhjdev@gmail.com>
2022-03-02 17:11:30 +0900

    Primality Test, 소인수분해

---
+++ 11600/src/BOJ11653.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11653 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			int n = Integer.parseInt(br.readLine());
+			
+			for(int i=2; i <= Math.sqrt(n); i++) {
+				while(n % i == 0) {
+					sb.append(i).append("\n");
+					n /= i;
+				}
+			}
+			
+			if(n != 1) {
+				sb.append(n);
+			}
+			
+			System.out.println(sb);
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
