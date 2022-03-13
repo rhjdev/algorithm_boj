25fd2deb3e668fe6c6d4e040bcb80078730c0ba4
rhjdev <rhjdev@gmail.com>
2022-03-14 08:48:02 +0900

    Arithmetic, 홍익대학교

---
+++ 16300/src/BOJ16394.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ16394 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		
+		int var = N - 1946;
+		System.out.println(var);
+	}
+}
