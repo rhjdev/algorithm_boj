44abd039ed9d3ae7df2abdeb61dae8601cbd142f
rhjdev <rhjdev@gmail.com>
2022-04-04 14:00:51 +0900

    Implementation, String, 단어 길이 재기

---
+++ 02700/src/BOJ2743.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2743 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			String input = br.readLine();
+			bw.write(String.valueOf(input.length()));
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
