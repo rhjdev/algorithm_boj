8b1c48a05417fb2bbbb99c52fb7d8fc039202592
rhjdev <rhjdev@gmail.com>
2022-04-11 16:43:19 +0900

    Mathematics, Implementation, Arithmetic, A+B - 8

---
+++ 11000/src/BOJ11022.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ11022 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		try {
+			int t = Integer.parseInt(br.readLine());
+			
+			for(int i=1; i <= t; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = a + b;
+				bw.append("Case #" + i + ": ")
+				  .append(a + " + " + b + " = " + c + "\n");
+			}
+			bw.flush();
+			bw.close();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
