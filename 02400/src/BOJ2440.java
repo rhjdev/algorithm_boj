95cda4c32deda8f4eec66225afd598cece3dfe8e
rhjdev <rhjdev@gmail.com>
2022-04-13 23:03:00 +0900

    Implementation, 별 찍기 - 3

---
+++ 02400/src/BOJ2440.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ2440 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i=0; i < n; i++) {
+			for(int j=i; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
