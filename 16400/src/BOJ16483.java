a3d0a75f5261617e48862d984217be56549689f3
rhjdev <rhjdev@gmail.com>
2022-04-18 16:17:54 +0900

    Geometry, Pythagoras Theorem, 접시 안의 원

---
+++ 16400/src/BOJ16483.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ16483 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int T = Integer.parseInt(br.readLine());
+			
+			/* a^2 - b^2 = (T/2) */
+			double radius = (double) T / 2.0;
+			double result = Math.pow(radius, 2);
+			
+			bw.write(String.valueOf(Math.round(result)));
+			bw.flush();
+			bw.close();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
