cdacd84ebbaf6b77e2b9393b63ca6227372bd33c
rhjdev <rhjdev@gmail.com>
2022-07-15 21:52:41 +0900

    Implementation, String, 그대로 출력하기

---
+++ 11700/src/BOJ11718.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11718 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while((input = br.readLine()) != null) {
+			System.out.printf("%s\n", input);
+		}
+	}
+}
