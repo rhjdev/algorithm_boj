229922d55121d2b68e67065358d6be07c079793c
rhjdev <rhjdev@gmail.com>
2023-05-30 07:32:19 +0900

    Implementation, 별 찍기 - 7

---
+++ 02400/src/BOJ2444.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2444 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0; i < N; i++) {
+			for(int j=0; j < N-1-i; j++) {
+				bw.append(" ");
+			}
+			for(int j=0; j < 2*i+1; j++) {
+				bw.append("*");
+			}
+			bw.append("\n");
+		}
+		for(int i=N-1; i > 0; i--) {
+			for(int j=0; j <= N-1-i; j++) {
+				bw.append(" ");
+			}
+			for(int j=2*i-1; j > 0; j--) {
+				bw.append("*");
+			}
+			bw.append("\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
