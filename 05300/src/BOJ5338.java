c6147f5dc88945d11d15ddcd3a7c92e7b3c4a378
rhjdev <rhjdev@gmail.com>
2023-09-19 19:48:43 +0900

    Implementation, 마이크로소프트 로고

---
+++ 05300/src/BOJ5338.java
@@ -0,0 +1,12 @@
+
+public class BOJ5338 {
+
+	public static void main(String[] args) {
+
+		System.out.printf("%s", "       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}
